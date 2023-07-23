613394e1359fee7bca7bdf03e5654755960c2e18
iheb meftah <ihebmeftah666@gmail.com>
2023-07-23 12:44:52 +0100

    favicon + web title

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ import 'app/routes/app_pages.dart';
 void main() {
   runApp(
     GetMaterialApp(
-      title: "Application",
+      title: "Iheb Meftah",
       initialRoute: AppPages.INITIAL,
       getPages: AppPages.routes,
     ),
