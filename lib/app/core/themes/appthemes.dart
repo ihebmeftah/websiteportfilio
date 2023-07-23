4fdb7f1b728cb9188fd6806a4ce8804c4956c368
iheb meftah <ihebmeftah666@gmail.com>
2023-07-23 12:55:30 +0100

    add screen utils / theme

---
+++ lib/app/core/themes/appthemes.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AppThemes {
+  static final light = ThemeData(
+    brightness: Brightness.light,
+  );
+
+  static final dark = ThemeData(
+    brightness: Brightness.dark,
+  );
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import 'package:get/get.dart';
+import 'package:websiteportfilio/app/core/themes/appthemes.dart';
 
 import 'app/routes/app_pages.dart';
 
 void main() {
   runApp(
-    GetMaterialApp(
-      title: "Iheb Meftah",
-      initialRoute: AppPages.INITIAL,
-      getPages: AppPages.routes,
-    ),
+    ScreenUtilInit(
+        designSize: const Size(360, 690),
+        minTextAdapt: true,
+        splitScreenMode: true,
+        builder: (context, child) {
+          return GetMaterialApp(
+            title: "Iheb Meftah",
+            initialRoute: AppPages.INITIAL,
+            getPages: AppPages.routes,
+            theme: AppThemes.light,
+            darkTheme: AppThemes.dark,
+            home: child,
+          );
+        }),
   );
 }
